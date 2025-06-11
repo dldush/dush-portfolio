c5817ed38c9306f7751975af67ce57b021cf379b
이연호 <leecarus0301@naver.com>
2025-06-11 12:15:44 +0900

    [Design] main page design

--- src/App.tsx
+++ src/App.tsx
@@ -3,76 +3,103 @@ import { motion } from "framer-motion";
 function App() {
   return (
     <>
-      <header className="relative w-full h-screen flex justify-center items-center">
-        <div className="relative w-full h-full flex justify-center items-center">
-          <motion.h1
-            initial={{ y: -100, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-            transition={{ duration: 0.8 }}
-            className="absolute text-[clamp(2rem,16vw,20rem)] font-['Impact'] z-10 top-[20%] -translate-y-1/2 text-[#c71f1a]"
-          >
+      <header className="flex flex-col w-full h-screen justify-start items-center">
+        <div className="w-full h-full flex flex-col justify-start items-center">
+          <h1 className="text-[clamp(2rem,19vw,20rem)] font-bold text-primary font-['Impact']">
             LEE YEONHO
-          </motion.h1>
-          <motion.img
-            initial={{ scale: 0.8, opacity: 0 }}
-            animate={{ scale: 1, opacity: 1 }}
-            transition={{ duration: 0.8, delay: 0.2 }}
+          </h1>
+          <img
             src="/images/profile.png"
             alt="profile"
-            className="absolute w-[70%] h-[70%] object-cover z-20"
+            className="absolute mt-50 w-[70%] h-[70%] object-cover z-20"
           />
-          <motion.div
-            initial={{ y: 100, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-            transition={{ duration: 0.8, delay: 0.4 }}
-            className="flex flex-row justify-between w-full z-10"
-          >
+          <div className="flex flex-row justify-between w-full z-10 px-27">
             <div className="flex flex-col">
               <div className="flex flex-col">
-                <h2 className="text-2xl font-bold text-[#c71f1a] font-['Impact']">
+                <h2 className="text-2xl font-bold text-primary font-['Impact']">
                   Kyunghee Univ.
                 </h2>
-                <h3 className="text-lg text-[#c71f1a] font-['Impact']">
+                <h3 className="text-lg text-primary font-['Impact']">
                   Economics
                 </h3>
-                <h3 className="text-lg text-[#c71f1a] font-['Impact']">
-                  2023.03 ~ 2027.02
+                <h3 className="text-secondary font-['Impact']">
+                  2015.03 ~ 2022.02
                 </h3>
               </div>
               <div className="flex flex-col">
-                <h2 className="text-2xl font-bold text-[#c71f1a] font-['Impact']">
+                <h2 className="text-2xl font-bold text-primary font-['Impact']">
                   AOMG
                 </h2>
-                <h3 className="text-lg text-[#c71f1a] font-['Impact']">
+                <h3 className="text-lg text-primary font-['Impact']">
                   A&R/Marketing
                 </h3>
-                <h3 className="text-lg text-[#c71f1a] font-['Impact']">
-                  2025.03 ~ 2025.06
+                <h3 className="text-secondary font-['Impact']">
+                  2021.12 ~ 2022.08
                 </h3>
               </div>
               <div className="flex flex-col">
-                <h2 className="text-2xl font-bold text-[#c71f1a] font-['Impact']">
+                <h2 className="text-2xl font-bold text-primary font-['Impact']">
                   SAMSUNG SW Academy
                 </h2>
-                <h2 className="text-2xl font-bold text-[#c71f1a] font-['Impact']">
+                <h2 className="text-2xl font-bold text-primary font-['Impact']">
                   For Youth
                 </h2>
-                <h3 className="text-lg text-[#c71f1a] font-['Impact']">
-                  2025.03 ~ 2025.06
+                <h3 className="text-secondary font-['Impact']">
+                  2024.06 ~ 2025.06
                 </h3>
               </div>
             </div>
             <div className="flex flex-col">
-              <h2 className="text-2xl font-bold text-[#c71f1a] font-['Impact']">
+              <a
+                href="https://github.com/dldush"
+                target="_blank"
+                rel="noopener noreferrer" //NOTE - window.opener 방지
+                className="text-2xl font-bold text-primary font-['Impact']"
+              >
                 Github
-              </h2>
-              <h2 className="text-2xl font-bold text-[#c71f1a] font-['Impact']">
+              </a>
+              <a
+                href="https://dush.tistory.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-2xl font-bold text-primary font-['Impact']"
+              >
                 Blog
-              </h2>
+              </a>
             </div>
-          </motion.div>
+          </div>
+        </div>
+        <div className="flex flex-row items-center justify-center w-full z-30">
+          <div className="text-tertiary font-['Impact'] flex flex-row gap-13 text-4xl mb-24 w-full justify-center [-webkit-text-emphasis:none]">
+            <span>f</span>
+            <span>r</span>
+            <span>o</span>
+            <span>n</span>
+            <span>t</span>
+            <span>-</span>
+            <span>e</span>
+            <span>n</span>
+            <span>d</span>
+            <span> </span>
+            <span>d</span>
+            <span>e</span>
+            <span>v</span>
+            <span>e</span>
+            <span>l</span>
+            <span>o</span>
+            <span>p</span>
+            <span>e</span>
+            <span>r</span>
+          </div>
         </div>
       </header>
+      <main className="flex flex-col w-full h-screen justify-start items-center px-20 pt-10">
+        <div className="flex flex-col w-full h-full justify-start items-center">
+          <h1 className="text-4xl font-bold text-primary font-['Impact']">
+            About
+          </h1>
+        </div>
+      </main>
     </>
   );
 }
